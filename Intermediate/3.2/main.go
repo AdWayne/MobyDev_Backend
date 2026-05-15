86213cc25f88ba5ffa05f652636ec316cf8828c3
AdWayne <batyawayne@gmail.com>
2026-05-15 20:34:00 +0500

    Stroki

--- Intermediate/3.2/main.go
+++ Intermediate/3.2/main.go
@@ -2,123 +2,108 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	//1
-	var text string
-	text = "Golang"
+	// Задание 1
+	var text string = "Программирование на Go"
 	const author string = "Azamat"
+
 	fmt.Printf("Автор %s написал: %s\n", author, text)
 	fmt.Println("Длина текста:", len(text))
 	fmt.Println()
 
-	//2
-	var message string = "Привет, мир!"
+	// Задание 2
+	var message string = "Я учу Golang язык программирование"
+
 	if message == "" {
-		fmt.Println("Строка пустая")
+		fmt.Println("Строка пуста")
 	} else {
 		fmt.Println("Строка не пустая")
 	}
-	fmt.Println("Длина текста:", len(message))
+
+	fmt.Println("Длина строки:", len(message))
 	fmt.Println()
 
-	//3
+	// Задание 3
 	var input string
-	fmt.Print("Введите строку: ")
+	fmt.Print("Введите слово: ")
 	fmt.Scan(&input)
+
 	if len(input) < 5 {
 		fmt.Println("Слишком короткое слово")
-	} else if len(input) >= 5 && len(input) <= 10 {
+	} else if len(input) <= 10 {
 		fmt.Println("Нормальная длина")
 	} else {
 		fmt.Println("Слишком длинное слово")
 	}
 	fmt.Println()
 
-	//4
-	word := "Programma"
+	// Задание 4
+	var word string = "Golang"
 
-	first := word[0]
-	last := word[len(word)-1]
+	fmt.Println("Первый символ:", string(word[0]))
+	fmt.Println("Последний символ:", string(word[len(word)-1]))
 
-	fmt.Printf("Первый символ: %c\n", first)
-	fmt.Printf("Последний символ: %c\n", last)
-
+	fmt.Println("Все символы:")
 	for i := 0; i < len(word); i++ {
-		fmt.Printf("%c\n", word[i])
+		fmt.Println(string(word[i]))
 	}
 
-	fmt.Println("Длина строки:", len(word))
+	fmt.Println("Длина слова:", len(word))
+	fmt.Println()
 
-	//5
-	sentence := "Hi, salam mister"
+	// Задание 5
+	var sentence string = "Мама"
+
 	countA := 0
-
 	for _, ch := range sentence {
-		if ch == 'a' || ch == 'A' || ch == 'а' || ch == 'А' {
+		if ch == 'а' || ch == 'А' {
 			countA++
 		}
 	}
 
-	fmt.Printf("В строке %d символов и %d букв a\n",
-		len(sentence),
-		countA,
-	)
+	fmt.Printf("В строке %d символов и %d букв а\n", len(sentence), countA)
 	fmt.Println()
 
-	//6
+	// Задание 6
 	var soz string
-
-	fmt.Printf("Введите строку: ")
-	fmt.Scanln(&soz)
+	fmt.Print("Проверка палиндрома: ")
+	fmt.Scan(&soz)
 
 	var reversed string
-
 	for i := len(soz) - 1; i >= 0; i-- {
 		reversed += string(soz[i])
 	}
-
-	fmt.Println("Исходная строка:", soz)
-	fmt.Println("Перевернутая строка:", reversed)
 
 	if soz == reversed {
 		fmt.Println("Это палиндром")
 	} else {
 		fmt.Println("Это не палиндром")
 	}
+
+	fmt.Println("Исходная строка:", soz)
+	fmt.Println("Перевёрнутая строка:", reversed)
 	fmt.Println()
 
-	//8
-	var soilem string
+	// Задание 7
+	var line string
+	fmt.Print("Введите текст: ")
+	fmt.Scanln()
+	fmt.Scanln(&line)
 
-	fmt.Print("Введите строку: ")
-	fmt.Scanln(&soilem)
+	length := len(line)
+	words := len(strings.Fields(line))
 
-	length := len(soilem)
+	vowels := "aeiouyAEIOUY"
+	countVowels := 0
 
-	words := 1
-	for i := 0; i < len(soilem); i++ {
-		if soilem[i] == ' ' {
-			words++
+	for i := 0; i < len(line); i++ {
+		if strings.ContainsRune(vowels, rune(line[i])) {
+			countVowels++
 		}
 	}
 
-	vowels := 0
-	vowelLetters := "aeiouyAEIOUY"
-
-	for i := 0; i < len(soilem); i++ {
-		for j := 0; j < len(vowelLetters); j++ {
-			if soilem[i] == vowelLetters[j] {
-				vowels++
-			}
-		}
-	}
-
-	fmt.Printf(
-		"Длина строки: %d, слов: %d, гласных: %d\n",
-		length,
-		words,
-		vowels,
-	)
+	fmt.Printf("Длина строки: %d, слов: %d, гласных: %d\n", length, words, countVowels)
 }
